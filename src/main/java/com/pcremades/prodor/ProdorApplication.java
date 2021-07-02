9958dad6ac5bff8a5063acb7082f46d3e2ec3cba
Pablo Cremades Serra <pablocremadesserra@gmail.com>
2021-07-02 14:28:55 +0300

    First commit

---
+++ src/main/java/com/pcremades/prodor/ProdorApplication.java
@@ -0,0 +1,13 @@
+package com.pcremades.prodor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pcremades/prodor/ProdorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcremades.prodor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
